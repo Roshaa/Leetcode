1b7f4b4d89ec65a40cf0f3079cc09105d8fd9cfb
Ricardo Rocha <48363753+Roshaa@users.noreply.github.com>
2025-11-23 14:44:20 +0000

    .

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,16 +1,26 @@
-﻿var s = new Solution();
+﻿Solution s = new Solution();
 
 public class Solution
 {
-    public int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+    public int[] LeftRightDifference(int[] nums)
     {
-        int hoursMet = 0;
+        int[] leftSum = new int[nums.Length];
 
-        for (int i = 0; i < hours.Length; i++)
-        {
-            if (hours[i] >= target) hoursMet++;
-        }
-        
-        return hoursMet;
+        for (int i = 1; i < nums.Length; i++)
+            leftSum[i] = leftSum[i - 1] + nums[i - 1];
+
+
+        int[] rightSum = new int[nums.Length];
+
+        for (int i = nums.Length - 2; i >= 0; i--)
+            rightSum[i] = rightSum[i + 1] + nums[i + 1];
+
+
+        int[] answer = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+            answer[i] = Math.Abs(leftSum[i] - rightSum[i]);
+
+        return answer;
     }
 }
