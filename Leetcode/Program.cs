7053d92421231e352bfae0cf182ceb7f0aef2ece
Ricardo Rocha <48363753+Roshaa@users.noreply.github.com>
2025-12-06 17:15:58 +0000

    leetcode101

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,24 +1,12 @@
 ﻿public class Solution
 {
-    public int[] FindIntersectionValues(int[] nums1, int[] nums2)
+    public int PrefixCount(string[] words, string pref)
     {
-        int left = 0;
-        int right = 0;
+        int counter = 0;
 
-        for (int i = 0; i < nums1.Length; i++)
-        {
-            if (nums2.Contains(nums1[i])) left++;
-        }
+        foreach (string s in words)
+            if (s.StartsWith(pref)) counter++;
 
-        for (int i = 0; i < nums2.Length; i++)
-        {
-            if (nums1.Contains(nums2[i])) right++;
-        }
-
-        int[] ans = new int[2];
-        ans[0] = left;
-        ans[1] = right;
-
-        return ans;
+        return counter;
     }
 }
