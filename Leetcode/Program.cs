131a5ed55a752640f05cef218c2d3dbb8a188d15
Ricardo Rocha <48363753+Roshaa@users.noreply.github.com>
2026-01-18 19:07:44 +0000

    .

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,21 +1,7 @@
-﻿Solution s = new Solution();
-
-public class Solution
+﻿public class Solution
 {
-    public int MaxProfit(int[] prices)
+    public int Jump(int[] nums)
     {
-        int maxProfit = 0;
-        int minCost = prices[0];
 
-        for (int i = 1; i < prices.Length; i++)
-        {
-            int profit = prices[i] - minCost;
-            if (profit > 0 && profit > maxProfit) maxProfit = profit;
-
-            if (prices[i] < minCost)
-                minCost = prices[i];
-        }
-
-        return maxProfit;
     }
 }
