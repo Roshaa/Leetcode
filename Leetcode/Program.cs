e6ef1ea98dcc45daff0f7900167d657d533eb479
Ricardo Rocha <48363753+Roshaa@users.noreply.github.com>
2026-01-25 17:21:25 +0000

    .

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,7 +1,29 @@
 ﻿public class Solution
 {
-    public int Jump(int[] nums)
+    public bool UniqueOccurrences(int[] arr)
     {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
 
+        foreach (int number in arr)
+        {
+            if (dict.ContainsKey(number))
+            {
+                dict[number]++;
+            }
+            else
+            {
+                dict[number] = 1;
+            }
+        }
+
+        HashSet<int> seen = new HashSet<int>();
+
+        foreach (int count in dict.Values)
+        {
+            if (!seen.Add(count))
+                return true;
+        }
+
+        return false;
     }
 }
