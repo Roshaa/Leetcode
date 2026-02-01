7c603a724a11b8b608fccf7a40660a66e450ab8c
Ricardo Rocha <48363753+Roshaa@users.noreply.github.com>
2026-02-01 18:16:19 +0000

    ...

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,21 +1,42 @@
-﻿public class Solution
+﻿Solution s = new Solution();
+//int result = s.Leet(7, new int[] { 2, 3, 1, 2, 4, 3 });
+int result = s.Leet(7, new int[] { 10, 2, 3 });
+//int result = s.Leet(11, new int[] { 1, 1, 1, 1, 1, 1, 1, 1 });
+
+
+public class Solution
 {
-    public IList<IList<string>> LeetCodeProblem(string[] strs)
+    public int Leet(int target, int[] nums)
     {
-        Dictionary<string, List<string>> map = new Dictionary<string, List<string>>();
+        int answer = int.MaxValue;
+        int currentSum = nums[0];
+        int leftPointer = 0;
 
-        foreach (string word in strs)
+        if (currentSum >= target)
+            return 1;
+
+        for (int rightPointer = 1; rightPointer < nums.Length; rightPointer++)
         {
-            char[] chars = word.ToCharArray();
-            Array.Sort(chars);
-            string key = new string(chars);
+            currentSum += nums[rightPointer];
 
-            if (!map.ContainsKey(key))
-                map[key] = new List<string>();
+            if (currentSum >= target)
+            {
+                while (currentSum >= target && leftPointer <= rightPointer)
+                {
+                    int diff = rightPointer - leftPointer + 1;
 
-            map[key].Add(word);
+                    if (diff < answer)
+                        answer = diff;
+
+                    currentSum -= nums[leftPointer];
+                    leftPointer++;
+
+                }
+            }
+
+
         }
 
-        return map.Values.ToList<IList<string>>();
+        return answer == int.MaxValue ? 0 : answer;
     }
 }
